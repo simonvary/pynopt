978e4b15673f2ce7924455904adfb3154ead1ea1
SimonVary <vary.simon@gmail.com>
2022-01-21 12:14:11 +0100

    Adding entry-wise sampling

--- nopt/transforms/__init__.py
+++ nopt/transforms/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
     "FastJLT",
     "LinearMatrix",
+    "EntryWise"
   #  "Wavelet2"
 ]
 
 from .fast_jlt import FastJLT
 from .linear_transform import LinearMatrix
+from .entry_wise import EntryWise
 #from .wavelet2 import Wavelet2
--- nopt/transforms/entry_wise.py
+++ nopt/transforms/entry_wise.py
@@ -12,35 +12,40 @@ from nopt.transforms.transform import Transform
 from scipy.sparse.linalg import LinearOperator 
 
 
-class LinearMatrix(Transform):
+class EntryWise(Transform):
     """
-    Linear transform based on a numpy array
+    Entry-wise transform on a sparse mask
+
     # should take the scipy LinearOperator for inheritence
-    """
-    
-    def __init__(self, matrix, *args, **kwargs):
+    """  
+    def __init__(self, ind, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._matrix = matrix
+        self._ind = ind
 
         if self.shape_input == None:
-            self.shape_input = (matrix.shape[1], 1)
+            self.shape_input = (matrix.shape[0], 1)
         if self.shape_output == None:
-            self.shape_output= (matrix.shape[0], 1)
+            self.shape_output= (matrix.shape[1], 1)
 
     # Function to apply the transform.
     def matvec(self, x):
         # change x from the self.shape_input to a vector
-        return np.matmul(self._matrix, x.flatten())
+        _x = x.copy()
+        ind_del = np.ones(x.shape, dtype=bool)
+        ind_del[self._ind] = False
+        _x[ind_del] = 0
+        return _x
 
     # Function to apply adjoint/backward transform.
     def rmatvec(self, y):
-        # change result from a vector to the self.shape_input 
+        '''
+            fill in the entries of a zero matrix of a shape
+        '''
         return np.matmul(self._matrix.transpose(), y).reshape(self.shape_input)
 
     # Function to apply the transform.
     def matmat(self, x):
-        # change x from the self.shape_input to a vector
-        # check that x is a matrix
+        # Todo: apply matvec in vmap
         return np.matmul(self._matrix, x)
 
     # Function to apply adjoint/backward transform.
