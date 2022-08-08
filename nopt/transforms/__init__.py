d9193b9f7a518977e14c4d4cb64de8359e21f198
Simon Vary <vary.simon@gmail.com>
2022-08-08 20:11:43 +0200

    cleanup

--- nopt/transforms/__init__.py
+++ nopt/transforms/__init__.py
@@ -1,15 +1,9 @@
 __all__ = [
     "FastJLT",
     "EntryWise",
-    "TensorLinearOperator",
     "CompositeTransform"
-  #  "Wavelet2"
 ]
 
 from .fast_jlt import FastJLT
 from .entry_wise import EntryWise
-from .tensor_linear_operator import TensorLinearOperator
 from .composite_transform import CompositeTransform
-
-
-## Transforms always go from an array to a vector
