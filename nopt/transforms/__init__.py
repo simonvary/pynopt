75060b37cedba5d1bb7e0ee48fee4a78577c0187
Simon Vary <vary.simon@gmail.com>
2022-09-26 13:53:53 +0200

    update

--- nopt/transforms/__init__.py
+++ nopt/transforms/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
     "FastJLT",
     "EntryWise",
-    "CompositeTransform"
+    "CompositeTransform",
+    "Trajectorlet"
 ]
 
 from .fast_jlt import FastJLT
 from .entry_wise import EntryWise
 from .composite_transform import CompositeTransform
+from .trajectorlet import Trajectorlet
